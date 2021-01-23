890af03cb8d42cd1d4ca8bad4dbcd643c3d6bd95
조준명 <a8456452@naver.com>
2021-01-23 22:17:27 +0900

    app. readme update

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
-import React, { Suspense } from 'react';
+import React from 'react';
 import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import PageWrapper from './components/PageWrapper';
 import PageTransitionWapper from './components/PageTransitionWapper';
-import FallBackUi from './components/Fallback';
 
 const FirstPage = React.lazy(() => import('./pages/Index'));
 
@@ -11,15 +10,13 @@ const App = () => {
     <PageWrapper>
       <Router>
         <PageTransitionWapper>
-          <Suspense fallback={<FallBackUi />}>
-            <Switch>
-              {/* First Page */}
-              <Route exact path="/"><FirstPage /></Route>
+          <Switch>
+            {/* First Page */}
+            <Route exact path="/"><FirstPage /></Route>
 
-              {/* Not Found Page */}
-              <Route><FirstPage /></Route>
-            </Switch>
-          </Suspense>
+            {/* Not Found Page */}
+            <Route><FirstPage /></Route>
+          </Switch>
         </PageTransitionWapper>
       </Router>
     </PageWrapper>
